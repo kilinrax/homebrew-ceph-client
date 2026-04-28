0f98f6c4dab191d3fc96850148bacf9599e806e9
Alex Bowley <alex@openimp.com>
2026-04-28 17:32:08 +0300

    Remove arm64, release name only contains 'sonoma'

--- ceph-client.rb
+++ ceph-client.rb
@@ -8,7 +8,7 @@ class CephClient < Formula
 bottle do
     rebuild 1
     root_url "https://github.com/kilinrax/homebrew-ceph-client/releases/download/reef-18.2.8-1/"
-    sha256 cellar: :any, arm64_sonoma: "sha256:326e324477fb627379d3b8b5eca6c916c049e95b23ed5c77ff385553e1bb9e33"
+    sha256 cellar: :any, sonoma: "sha256:326e324477fb627379d3b8b5eca6c916c049e95b23ed5c77ff385553e1bb9e33"
   end
 
   # depends_on "osxfuse"
@@ -57,7 +57,7 @@ bottle do
 
   resource "PyYAML" do
     url "https://files.pythonhosted.org/packages/54/ed/79a089b6be93607fa5cdaedf301d7dfb23af5f25c398d5ead2525b063e17/pyyaml-6.0.2.tar.gz"
-    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e"
+    sha256 "d584d9ec91ad65861cc08d42e834324ef890a082e591037abe114850ff7bbc3e2"
   end
 
   #resource "wcwidth" do
